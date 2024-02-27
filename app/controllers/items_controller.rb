5fb075873194f957bb24f50aa2d56ed509d6faf5
Anna Putt anna.putt@lewagon.orgexec zs <anna.putt@lewagon.org>
2024-02-27 15:40:26 +0100

    cloudinary images for items to rent

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,5 @@
 class ItemsController < ApplicationController
+  def item_params
+    params.require(:item).permit(:name, :photo)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   has_many :bookings
   belongs_to :user
+  has_one_attached :photo
 
   validates :name, presence: true
   validates :description, presence: true
