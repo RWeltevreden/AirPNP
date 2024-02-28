65090ab42732814bce0d46bacb69c4d7e9b12688
Anna Karsten <anna-karsten@hotmail.com>
2024-02-28 15:53:11 +0100

    homepagefrontend

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @items = Item.all.limit(3)
+    @items = Item.all.limit(4)
   end
 end
