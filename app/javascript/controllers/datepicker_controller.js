c185109842a4ddf0b7cb3cf78dbbac40e811d126
Anna Putt anna.putt@lewagon.orgexec zs <anna.putt@lewagon.org>
2024-03-01 10:40:47 +0100

    re-adding from yesterday

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -3,10 +3,10 @@ import flatpickr from "flatpickr";
 
 export default class extends Controller {
   connect() {
-    flatpickr(this.element, {
-        minDate: "today",
-        dateFormat: "d.m.Y",
+    flatpickr(this.element
+        // minDate: "today",
+        // dateFormat: "d.m.Y",
         // maxDate: new Date().fp_incr(7)
-    });
+    );
   }
 }
